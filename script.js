f850934da82fd143a7536a6a682a41ff6b08de22
TGOLD <thomaswgold90@gmail.com>
2022-07-22 19:28:27 -0600

    working on oneCall PAI request

--- script.js
+++ script.js
@@ -1,27 +1,65 @@
 
 var searchBtn = $("#search-btn");
 var cityInput = $("cityInput");
+var cityText = ("#citytext");
+var temp = $("#temp");
+var wind = $("#wind");
+var humidity = $("#humidity");
+var uvIndex = $("#uv");
 
+var lon;
+var lat;
+var coord;
 var city;
 var APIKey = "cb222b8fc6b876ca425d882c9612884c";
-// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey; //&units=standard add this later when we know if works
 
-// "http://api.openweathermap.org/data/2.5/weather?q=chicago&appid=cb222b8fc6b876ca425d882c9612884c" this works
 
 function citySearch(){
     city = $(this).siblings("input").val().trim();
     cityInput = "";
     console.log(city);
 
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    // var getCoords = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + APIKey;
 
-    fetch(queryURL).then(function (response){
+    // fetch(getCoords).then(function (response){
+    //     return response.json()
+    // }).then(function(data){
+    // console.log(data)
+    // });
+
+    // var queryURL = "https://api.openweathermap.org/data/2.5/onecall?q=" + city + "&appid=" + APIKey;
+
+    var getCity = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey +"&units=imperial";
+
+    fetch(getCity).then(function (response){
         return response.json()
     }).then(function(data){
     console.log(data)
+
+    // lon = data.coord.lon.val;
+    // lat = data.coord.lon.val;
+    coord = data.coord;
+    console.log(coord);
+    
+    
+
+    var oneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coord + "&exclude=minutely,hourly&appid=" + APIKey;
+
+    fetch(oneCall).then(function (response){
+        return response.json()
+    }).then(function(data){
+    console.log(data)});
+
     });
+
+
 }
+
+//function to append city weather data to html
+
 
 searchBtn.click(citySearch)
 
+//function to save to local storage
 
+//for loop to 
