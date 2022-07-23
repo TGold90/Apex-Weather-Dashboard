111140b3cb8e63556db39a9d82d1e5061f992630
TGOLD <thomaswgold90@gmail.com>
2022-07-23 12:08:07 -0600

    city-data div filled out

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 var searchBtn = $("#search-btn");
 var cityInput = $("cityInput");
-var cityText = ("#citytext");
+var cityText = $("#citytext");
 var temp = $("#temp");
 var wind = $("#wind");
 var humidity = $("#humidity");
@@ -16,44 +16,42 @@ var APIKey = "cb222b8fc6b876ca425d882c9612884c";
 
 function citySearch(){
     city = $(this).siblings("input").val().trim();
-    cityInput = "";
+    cityInput.value = ""; //ask Eric
     console.log(city);
-
-    // var getCoords = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + APIKey;
-
-    // fetch(getCoords).then(function (response){
-    //     return response.json()
-    // }).then(function(data){
-    // console.log(data)
-    // });
-
-    // var queryURL = "https://api.openweathermap.org/data/2.5/onecall?q=" + city + "&appid=" + APIKey;
 
     var getCity = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey +"&units=imperial";
 
     fetch(getCity).then(function (response){
         return response.json()
     }).then(function(data){
-    console.log(data)
+        console.log(data)
 
-    // lon = data.coord.lon.val;
-    // lat = data.coord.lon.val;
-    coord = data.coord;
-    console.log(coord);
+        coord = data.coord;
+        lon = coord.lon;
+        lat = coord.lat;
+        // console.log(coord);
+        // console.log(lon);
+        // console.log(lat);
     
-    
 
-    var oneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coord + "&exclude=minutely,hourly&appid=" + APIKey;
+        var oneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=" + APIKey + "&units=imperial";
 
-    fetch(oneCall).then(function (response){
-        return response.json()
-    }).then(function(data){
-    console.log(data)});
+        fetch(oneCall).then(function (response){
+            return response.json()
+        }).then(function(fulldata){
+        console.log(fulldata)
 
+        //maybe put this in a separate function so i can eruse for card creation
+        cityText.text(data.name);
+        console.log(data.name);
+        temp.text("Temp: " + data.main.temp +"° F");
+        wind.text("Wind: " + data.wind.speed + " mph");
+        humidity.text("Humidity: " + data.main.humidity + " %")
+        uvIndex.text("UV Index: " + fulldata.current.uvi);
+
+        });
     });
-
-
-}
+};
 
 //function to append city weather data to html
 
@@ -63,3 +61,4 @@ searchBtn.click(citySearch)
 //function to save to local storage
 
 //for loop to 
+
