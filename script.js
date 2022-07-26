7d88e2555aaff8e311abbb20bfa646431c679f4f
TGOLD <thomaswgold90@gmail.com>
2022-07-26 14:25:17 -0600

    final

--- script.js
+++ script.js
@@ -12,8 +12,7 @@ var lat;
 var coord;
 var city;
 var location;
-// var state;
-// var country;
+
 var APIKey = "cb222b8fc6b876ca425d882c9612884c";
 var storedCity = JSON.parse(localStorage.getItem("storedCity")) || [];
 
@@ -30,27 +29,15 @@ if ($(this).attr("id") === "search-btn"){
     city = $(this).text();
 }
 
-    
-    console.log(city);
-
-    // var getCity = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey +"&units=imperial";
-
     var geoLoc = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + APIKey;
-
-    // "https://api.openweathermap.org/data/2.5/weather?q=:" + location + "&appid="{API key" incl state/country
 
     fetch(geoLoc).then(function (response){
         return response.json()
     }).then(function(data){
-        console.log(data)
 
         coord = data[0];
         lon = coord.lon;
         lat = coord.lat;
-        // console.log(coord);
-        // console.log(lon);
-        // console.log(lat);
-    
 
         var oneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=" + APIKey + "&units=imperial";
 
@@ -59,16 +46,17 @@ if ($(this).attr("id") === "search-btn"){
         }).then(function(fulldata){
         console.log(fulldata)
 
-        //maybe put this in a separate function so i can reuse for card creation
         let uvIndexNum = fulldata.current.uvi;
-
+        let icon = $("<img>");
         cityText.text(data[0].name);
         
         temp.text("Temp: " + fulldata.current.temp +"° F");
         wind.text("Wind: " + fulldata.current.wind_speed + " mph");
         humidity.text("Humidity: " + fulldata.current.humidity + " %");
         uvIndex.text("UV Index: " + uvIndexNum);
-
+        icon.attr("src", "http://openweathermap.org/img/wn/" + fulldata.daily[0].weather[0].icon + "@2x.png");
+        icon.addClass("icon");
+        $("#cityIcon").append(icon);
         if (uvIndexNum >= 7){
             uvIndex.addClass("severe");
         } else if (uvIndexNum < 7 && uvIndex >= 4){
@@ -89,27 +77,30 @@ if ($(this).attr("id") === "search-btn"){
 function renderFiveDay(fiveDay){
     for (let i=1; i < 6; i++){
         var card = $("<div>");
-        card.addClass("card text-left")
+        card.addClass("card text-left px-1")
         var img = $("<img>");
         img.addClass("card-img-top");
-        img.attr("src", "http://openweathermap.org/img/wn/" + fiveDay[i].weather[0].icon + "@2x.png"); //add same thing to current weather 
+        img.attr("src", "http://openweathermap.org/img/wn/" + fiveDay[i].weather[0].icon + "@2x.png");
         var cardBody = $("<div>");
         var cardTitle = $("<h4>");
         var temp = $("<p>");
+        var low = $("<p>");
         var wind = $("<p>");
         var humidity = $("<p>");
         cardBody.addClass("card-body");
         cardTitle.addClass("card-title");
         temp.addClass("card-text");
+        low.addClass("card-text");
         wind.addClass("card-text");
         humidity.addClass("card-text");
 
         cardTitle.text(moment.unix(fiveDay[i].dt).format("MM/DD/YYYY"));
-        temp.text("temp: " + fiveDay[i].temp.max);
-        wind.text("temp: " + fiveDay[i].wind_speed);
-        humidity.text("temp: " + fiveDay[i].humidity);
+        temp.text("High: " + fiveDay[i].temp.max + "° F");
+        low.text("Low: " + fiveDay[i].temp.min + "° F");
+        wind.text("Wind: " + fiveDay[i].wind_speed + "mph");
+        humidity.text("Humidity: " + fiveDay[i].humidity + "%");
 
-        card.append(img, cardBody, cardTitle, temp, wind, humidity);
+        card.append(img, cardBody, cardTitle, temp, low, wind, humidity);
         $("#forecast-div").append(card);
     }
     
@@ -123,14 +114,8 @@ function renderBtn (){
         btn.text(storedCity[i]);
         $("#button-div").append(btn);
     }
-    
 }
 
 renderBtn()
 $("#button-div").on("click", "button", citySearch)
 searchBtn.click(citySearch)
-
-//function to save to local storage
-
-//for loop to gen cards
-
